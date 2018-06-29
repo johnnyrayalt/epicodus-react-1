465d1500df39cbf1bc771afbb4e9f9fdf0046681
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-29 11:43:59 -0700

    add data types mapped

--- src/components/BeerDetails.jsx
+++ src/components/BeerDetails.jsx
@@ -4,13 +4,25 @@ import PropTypes from 'prop-types';
 function BeerDetails(props) {
   return(
     <div className='container'>
-      <h3 className='name'>{props.name}</h3>
+      <div className='brandInfo'>
+        <h3 className='name'>{props.name}</h3>
+        <h4 className='brand'>{props.brand}</h4>
+      </div>
+      <div className='type'>{props.type}</div>
+      <div className='price'>{props.price}</div>
+      <div className='alcContent'>{props.alcContent}</div>
+      <div className='pints'>{props.price}</div>
     </div>
   );
 }
 
-BeerDetails.PropTypes = {
-  name: PropTypes.string.isRequired
+BeerDetails.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  alcContent: PropTypes.number.isRequired,
+  pints: PropTypes.number
 };
 
 export default BeerDetails;
