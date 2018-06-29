96e8e2834aed7cdff3e50f8dcf54561638c7c11e
Johnny Ray Alt <johnnyrayalt@gmail.com>
2018-06-29 16:23:32 -0700

    style for splash page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,35 @@ import { Link } from 'react-router-dom';
 function Home() {
   return(
     <div className='container'>
-      <div className='welcomeHead'>Welcome!</div>
-      <div className='welcomeSubHead'>
-        <Link to='/beerlist'>Let's see what's on tap</Link>
+      <Link className='link' to='/beerlist' style={{ textDecoration: 'none' }}>
+      <div className='centerSplash'>
+        <div className='welcomeHead'>Welcome!</div>
+        <div className='welcomeSubHead'>
+          Let's see what's on tap
+        </div>
       </div>
+      </Link>
+      <style jsx>{`
+        .centerSplash {
+          text-align:center;
+          padding-top:30%;
+        }
+
+        .welcomeHead {
+          font-family: 'Righteous', cursive;
+          font-size:120px;
+          color:#fcedb2;
+          -webkit-text-stroke: 5px #af4134;
+        }
+
+        .welcomeSubHead {
+          font-family: 'Chango',sans-serif;
+          color:#df6a3b;
+          -webkit-text-stroke: 2px #af4134;
+          font-size:36px;
+        }
+
+            `}</style>
     </div>
   );
 }
